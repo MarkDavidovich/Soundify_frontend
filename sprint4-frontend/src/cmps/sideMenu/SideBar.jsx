55a490ce2bac3fe1cc07bf135c427ee5d2649aae
NadavZ101 <nadavzivyon@gmail.com>
2024-04-04 11:56:58 +0300

    SideBar cmp comments

--- sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
+++ sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
@@ -1,9 +1,13 @@
 //MAIN SIDEBAR CMP, WILL CONTAIN THE OTHER CMPS
 
+//! Link to HomePage
+//! Link to ThemesPage
+//! Render the  StationIndex ---> StationList
+
 import React from 'react'
 
-export  function SideBar() {
-  return (
-    <div>SideBar</div>
-  )
+export function SideBar() {
+    return (
+        <div>SideBar</div>
+    )
 }
