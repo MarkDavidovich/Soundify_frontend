4019c661fcfa61e6a70f9ddc93c46d8bc28ec970
Haim Braunstein <haim321@gmail.com>
2024-04-05 11:45:52 +0300

    Add home and search buttons and styled them

--- sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
+++ sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
@@ -3,17 +3,72 @@
 //! Link to HomePage
 //! Link to ThemesPage
 
-import React from 'react'
+import React, { useState } from 'react'
 import { SideIndex } from './SideIndex'
 
 export function SideBar() {
+
+    const [isWhiteFill, setIsWhiteFill] = useState(true)
+    const [toggleSvgSearch, setToggleSvgSearch] = useState(false)
+
+
+    const handleHomeButtonClick = () => {
+        setIsWhiteFill(prevState => !prevState)
+    }
+
+    const handleSearchButtonClick = () => {
+        setToggleSvgSearch(prevState => !prevState)
+    }
+
     return (
         <>
             <div className="side-bar flex column">SideBar
                 <div className="actions-btns">
-                    <button className="home-btn btn">
-                        <img className="home-btn" src="./public/icons/home-icon.png" title="home page" alt="" />
-                    </button>
+                    <div className="home-btn-container">
+                        <button className="home-btn btn" onClick={handleHomeButtonClick}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                style={{ width: "24px", height: "24px" }}
+                            >
+                                <path className='fill-home' d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"
+                                    fill={isWhiteFill ? "none" : "white"}
+                                    stroke="#ffffff"
+                                    strokeWidth="1"
+                                >
+                                </path>
+                            </svg>
+
+                        </button>
+                        <span>
+                            Home
+                        </span>
+                    </div>
+
+                    <div className="search-btn-container">
+                        <button onClick={handleSearchButtonClick}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                style={{ width: "24px", height: "24px" }}
+                            >
+                                <path
+                                    d="M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 0 1-2.077 5.816l4.344 4.344a1 1 0 0 1-1.414 1.414l-4.353-4.353a9.454 9.454 0 0 1-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z"
+                                    fill="#FFFFFF"
+                                />
+                                {toggleSvgSearch && (
+                                    <path
+                                        d="M15.356 10.558c0 2.623-2.16 4.75-4.823 4.75-2.664 0-4.824-2.127-4.824-4.75s2.16-4.75 4.824-4.75c2.664 0 4.823 2.127 4.823 4.75z"
+                                        fill="#FFFFFF"
+                                    />
+                                )}
+                            </svg>
+
+                        </button>
+                        <span>
+                            Search
+                        </span>
+                    </div>
                 </div>
                 <SideIndex />
             </div>
