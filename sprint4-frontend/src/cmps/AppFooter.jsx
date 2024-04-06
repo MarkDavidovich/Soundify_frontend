6cbf2f29b3394aba0a0a22f2ee53d1edb4a60ae9
Marchello92 <mark.davidovich@gmail.com>
2024-04-06 16:32:39 +0300

    Add play/pause display

--- sprint4-frontend/src/cmps/AppFooter.jsx
+++ sprint4-frontend/src/cmps/AppFooter.jsx
@@ -160,10 +160,18 @@ export function AppFooter() {
                     <button className="play-btn" onClick={() => {
                         play()
                     }}>
-                        <svg width="16" height="16" viewBox="0 0 16 16" ><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"
-                        >
-                        </path>
-                        </svg>
+                        {isPlaying ? (<svg width="17" height="17" viewBox="0 0 16 16">
+                            <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
+                            >
+                            </path>
+                        </svg>) : (<svg width="17" height="17" viewBox="0 0 16 16" >
+                            <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"
+                            >
+                            </path>
+                        </svg>)
+                        }
+
+
                     </button>
 
                     <div className="player-controls-right">
