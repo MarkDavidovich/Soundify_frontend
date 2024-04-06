1d99211a36f2ec655fd6f0446ea8ae95fd4be96b
Haim Braunstein <haim321@gmail.com>
2024-04-06 16:01:57 +0300

    Add duration svg in station details

--- sprint4-frontend/src/cmps/StationDetails.jsx
+++ sprint4-frontend/src/cmps/StationDetails.jsx
@@ -105,7 +105,12 @@ export function StationDetails() {
                         <span className="title">Title</span>
                         <span className="album">Album</span>
                         <span className="date">Date added</span>
-                        <span className="duration">⏲</span>
+                        <span className="duration">
+                            <svg data-encore-id="icon" role="img" aria-hidden="true" width="16" height="16" viewBox="0 0 16 16" class="Svg-sc-ytk21e-0 dYnaPI"><path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" fill='white' strokeWidth='0.3' stroke="white"></path>
+                                <path d="M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z"
+                                    stroke="white" strokeWidth='0.3' fill='white'>
+                                </path></svg>
+                        </span>
                     </div>
                     <ul>
                         {currStation.songs.map((song, idx) => (
