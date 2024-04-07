9d6ce0f438bf75250902760fcd021ffa828d7c24
NadavZ101 <nadavzivyon@gmail.com>
2024-04-07 09:53:05 +0300

    Style the edit modal with changing the svg in hover mode

--- sprint4-frontend/src/cmps/sideMenu/UpdateStation.jsx
+++ sprint4-frontend/src/cmps/sideMenu/UpdateStation.jsx
@@ -7,6 +7,7 @@ export function UpdateStation({ station, setIsOnUpdate, onUpdateStation, handleS
 
     const [stationToUpdate, setStationToUpdate] = useState(station)
     const [imagePreview, setImagePreview] = useState(null)
+    const [isHovering, setIsHovering] = useState(false)
 
     function handleChange(ev) {
         // ev.preventDefault()
@@ -62,7 +63,7 @@ export function UpdateStation({ station, setIsOnUpdate, onUpdateStation, handleS
                             </svg>
                         </button>
                     </div>
-                    <div className="content-container grid">
+                    <div className="content-container">
 
                         <input className="station-name"
                             type="text"
@@ -82,12 +83,32 @@ export function UpdateStation({ station, setIsOnUpdate, onUpdateStation, handleS
                             onChange={handleChange}
                         />
 
-                        <div className="station-img">
-                            {imagePreview && <img src={imagePreview} alt="Preview" className="image-preview" />}
+                        <div className="station-img"
+                            onMouseOver={() => setIsHovering(true)}
+                            onMouseOut={() => setIsHovering(false)}>
 
-                            <label htmlFor="image-upload" className="image-upload-btn">
-                                Upload Image
-                            </label>
+                            {imagePreview ? (
+                                <img src={imagePreview} alt="Preview" className="image-preview" />
+                            ) : (
+                                <label htmlFor="image-upload" className="image-upload-btn">
+
+                                    {isHovering ? (
+                                        <div className="svg-hover-txt">
+                                            <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24" className="Svg-sc-ytk21e-0 bHdpig">
+                                                <path d="M17.318 1.975a3.329 3.329 0 1 1 4.707 4.707L8.451 20.256c-.49.49-1.082.867-1.735 1.103L2.34 22.94a1 1 0 0 1-1.28-1.28l1.581-4.376a4.726 4.726 0 0 1 1.103-1.735L17.318 1.975zm3.293 1.414a1.329 1.329 0 0 0-1.88 0L5.159 16.963c-.283.283-.5.624-.636 1l-.857 2.372 2.371-.857a2.726 2.726 0 0 0 1.001-.636L20.611 5.268a1.329 1.329 0 0 0 0-1.879z">
+                                                </path>
+                                            </svg>
+                                            <span>Choose photo</span>
+                                        </div>
+                                    ) : (
+                                        <svg data-encore-id="icon" role="img" aria-hidden="true" data-testid="playlist" class="Svg-sc-ytk21e-0 iYxpxA" viewBox="0 0 24 24">
+                                            <path d="M6 3h15v15.167a3.5 3.5 0 1 1-3.5-3.5H19V5H8v13.167a3.5 3.5 0 1 1-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 1 0 1.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 1 0 1.5 1.5v-1.5z" fill="#b3b3af">
+                                            </path>
+                                        </svg>
+                                    )}
+
+                                </label>
+                            )}
                             <input
                                 type="file"
                                 id="image-upload"
