dbc1890c96c722c9a4e32123eca743ed3ed9584b
Marchello92 <mark.davidovich@gmail.com>
2024-04-07 09:30:05 +0300

    Add support for setting current station from station preview

--- sprint4-frontend/src/cmps/StationPreview.jsx
+++ sprint4-frontend/src/cmps/StationPreview.jsx
@@ -1,9 +1,18 @@
 
+import { useDispatch } from 'react-redux'
+import { getActionCurrStation } from '../../src/store/actions/player.actions'
+
 export function StationPreview({ station }) {
+
+    const dispatch = useDispatch()
+
+    function handleStationClick(station) {
+        dispatch(getActionCurrStation(station))
+    }
 
     return (
 
-        <article className="station-preview">
+        <article className="station-preview" onClick={handleStationClick(station)}>
             <div className="img-container">
                 <img className="station-img" src={station.imgUrl} alt={`${station.name}`} />
             </div>
