5051668e69fb5d00959f512c0c5b4d2b9bfeab65
NadavZ101 <nadavzivyon@gmail.com>
2024-04-07 12:11:59 +0300

    style the sort buttons with dynamic class and styles

--- sprint4-frontend/src/cmps/sideMenu/SideSort.jsx
+++ sprint4-frontend/src/cmps/sideMenu/SideSort.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
 
 
 export function SideSort({ sortBy, onSetSort }) {
 
+    const [selectedSort, setSelectedSort] = useState(sortBy.by)
+
     function handleSort(by) {
+        setSelectedSort(by)
+
         const updatedSort = { ...sortBy, by }
         onSetSort(updatedSort)
     }
 
+    function onSetToggleClick() {
+        setToggleClick(!toggleClick)
+    }
+
+    function setBtnClass() {
+        if (!toggleClick) return 'btn'
+        else return 'btn-clicked'
+    }
+
+    function getBtnClass(by) {
+        return selectedSort === by ? 'btn-clicked' : 'btn'
+    }
+
     return <div className="side-sort-container flex">
-        <button onClick={() => handleSort('station')}>Playlist</button>
-        <button onClick={() => handleSort('artist')}>Artists</button>
-    </div>
+        <button className={getBtnClass('station')} onClick={() => handleSort('station')}>Playlist</button>
+
+        <button className={getBtnClass('artist')} onClick={() => handleSort('artist')}>Artists</button>
+    </div >
 }
