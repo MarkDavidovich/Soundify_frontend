223fe136df36736a0a25e23d132bfaa3b99ce7bc
NadavZ101 <nadavzivyon@gmail.com>
2024-04-07 18:17:00 +0300

    cleaning code in some cmps

--- sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
+++ sprint4-frontend/src/cmps/sideMenu/SideBar.jsx
@@ -44,30 +44,33 @@ export function SideBar() {
                         </div>
                     </Link>
 
-                    <div className="search-btn-container" onClick={handleSearchButtonClick}>
-                        <button className="search-btn">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                style={{ width: "20px", height: "20px" }}
-                            >
-                                <path
-                                    d="M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 0 1-2.077 5.816l4.344 4.344a1 1 0 0 1-1.414 1.414l-4.353-4.353a9.454 9.454 0 0 1-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z"
-                                    fill="#b3b3b3"
-                                    stroke="#b3b3b3"
-                                    strokeWidth="1"
-                                />
-                                {toggleSvgSearch && (
+                    <Link className="search-link" to="/search">
+                        <div className="search-btn-container" onClick={handleSearchButtonClick}>
+                            <button className="search-btn">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    style={{ width: "20px", height: "20px" }}
+                                >
                                     <path
-                                        d="M15.356 10.558c0 2.623-2.16 4.75-4.823 4.75-2.664 0-4.824-2.127-4.824-4.75s2.16-4.75 4.824-4.75c2.664 0 4.823 2.127 4.823 4.75z"
-                                        fill="#FFFFFF"
+                                        d="M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 0 1-2.077 5.816l4.344 4.344a1 1 0 0 1-1.414 1.414l-4.353-4.353a9.454 9.454 0 0 1-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z"
+                                        fill="#b3b3b3"
+                                        stroke="#b3b3b3"
+                                        strokeWidth="1"
                                     />
-                                )}
-                            </svg>
+                                    {toggleSvgSearch && (
+                                        <path
+                                            d="M15.356 10.558c0 2.623-2.16 4.75-4.823 4.75-2.664 0-4.824-2.127-4.824-4.75s2.16-4.75 4.824-4.75c2.664 0 4.823 2.127 4.823 4.75z"
+                                            fill="#FFFFFF"
+                                        />
+                                    )}
+                                </svg>
 
-                        </button>
-                        <span className="span-search">Search</span>
-                    </div>
+                            </button>
+                            <span className="span-search">Search</span>
+                        </div>
+                    </Link>
+
                 </div>
                 <SideIndex />
             </div>
