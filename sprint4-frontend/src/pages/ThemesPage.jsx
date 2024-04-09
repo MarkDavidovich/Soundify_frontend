989a0044fbd4656757bd79e53f98680d794f9912
Haim Braunstein <haim321@gmail.com>
2024-04-09 16:11:06 +0300

    Styled add song from play list

--- sprint4-frontend/src/pages/ThemesPage.jsx
+++ sprint4-frontend/src/pages/ThemesPage.jsx
@@ -29,46 +29,62 @@ export function ThemesPage({ onSongSelect, currStation }) {
     onSongSelect(song)
   }
 
-  const handleFindMore = () => {
+  function handleFindMore() {
     setShowFindMore(true)
   }
 
   async function handleAddSongFromSearch(selectedSong) {
     const updatedStation = {
-        ...currStation,
-        songs: [...currStation.songs, selectedSong]
+      ...currStation,
+      songs: [...currStation.songs, selectedSong]
     };
     try {
-        await dispatch(updateStation(updatedStation))
-        showSuccessMsg('Song added successfully')
+      await dispatch(updateStation(updatedStation))
+      showSuccessMsg('Song added successfully')
     } catch (err) {
-        console.error('Failed to add song:', err)
+      console.error('Failed to add song:', err)
     }
-}
-
+  }
 
   return (
     <section className="main-view">
-      <button onClick={handleFindMore}>Find more</button>
+      <button className='find-more-btn' onClick={handleFindMore}>
+        {showFindMore ? null : <div className="find-more-text">Find more</div>}
+      </button>
       {showFindMore && (
-        <section>
-          <button onClick={() => setShowFindMore(false)}>X</button>
-          <h1>Let's find something for your playlist</h1>
-          <SearchSongs searchSongs={searchSongs} />
+        <section className=' add-song-container flex'>
+          <div className="search-song-container">
+            <h1 className='find-song'>Let's find something for your playlist</h1>
+            <SearchSongs searchSongs={searchSongs} />
+          </div>
           <div className="song-list">
             {songs?.map(song => (
-              <div className="song-preview" key={song.id}>
-                <h4>Title: {song.title}</h4>
-                <h4>Artist: {song.artist}</h4>
-                <h4>URL: {song.url}</h4>
-                <img src={song.imgUrl} alt="Song artwork" />
-                <h4>Duration: {song.duration}</h4>
-                <button onClick={() => handleAddSongFromSearch(song)}>Select Song</button>
+              <div className="song-preview flex" key={song.id}>
+                <div className="song-info">
+                  <div className="song-img-container flex">
+                    <img src={song.imgUrl} alt="Song artwork" />
+                  </div>
+                  <div className="song-desc flex">
+                    <span className='song-title'>
+                    {song.title}
+                    </span>
+                    <span className='song-artist'>
+                    <h4> {song.artist}</h4>
+                    </span>
+                    <div className="add-song">
+                      <button className="add-song-btn flex" onClick={() => handleAddSongFromSearch(song)}>
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
+          <button className='close-search-btn' onClick={() => setShowFindMore(false)}><svg data-encore-id="icon" role="img" aria-label="Close" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 eJsVCw"><path d="M3.293 3.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 0 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414z"></path></svg>
+          </button>
         </section>
       )}
     </section>
-  );
+  )
 }
