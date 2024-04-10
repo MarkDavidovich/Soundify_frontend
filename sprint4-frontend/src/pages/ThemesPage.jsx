c8100308d47bcdf3a86d1875f5fcdec098a4e746
Haim Braunstein <haim321@gmail.com>
2024-04-10 12:37:14 +0300

    Add search song to new playlist

--- sprint4-frontend/src/pages/ThemesPage.jsx
+++ sprint4-frontend/src/pages/ThemesPage.jsx
@@ -37,7 +37,7 @@ export function ThemesPage({ onSongSelect, currStation }) {
     const updatedStation = {
       ...currStation,
       songs: [...currStation.songs, selectedSong]
-    };
+    }
     try {
       await dispatch(updateStation(updatedStation))
       showSuccessMsg('Song added successfully')
@@ -48,11 +48,16 @@ export function ThemesPage({ onSongSelect, currStation }) {
 
   return (
     <section className="main-view">
-      <button className='find-more-btn' onClick={handleFindMore}>
-        {showFindMore ? null : <div className="find-more-text">Find more</div>}
-      </button>
-      {showFindMore && currStation.songs.length === 0 &&(
-        <section className=' add-song-container flex'>
+      {currStation.songs.length > 0 && (
+        <div>
+          <button className='find-more-btn' onClick={() => setShowFindMore(!showFindMore)}>
+            {showFindMore ? null : <div className="find-more-text">Find more</div>}
+          </button>
+        </div>
+      )}
+
+      {(showFindMore || currStation.songs.length === 0) && (
+        <section className='add-song-container flex'>
           <div className="search-song-container">
             <h1 className='find-song'>Let's find something for your playlist</h1>
             <SearchSongs searchSongs={searchSongs} />
@@ -81,10 +86,14 @@ export function ThemesPage({ onSongSelect, currStation }) {
               </div>
             ))}
           </div>
-          <button className='close-search-btn' onClick={() => setShowFindMore(false)}><svg data-encore-id="icon" role="img" aria-label="Close" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 eJsVCw"><path d="M3.293 3.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 0 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414z"></path></svg>
+          <button className='close-search-btn' onClick={() => setShowFindMore(false)}>
+            <svg data-encore-id="icon" role="img" aria-label="Close" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 eJsVCw">
+              <path d="M3.293 3.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 0 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414z"></path>
+            </svg>
           </button>
         </section>
       )}
     </section>
   )
+
 }
