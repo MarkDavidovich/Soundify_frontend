906caa2790c8667525bc26c5d045acf4b5c9812d
Marchello92 <mark.davidovich@gmail.com>
2024-04-11 12:01:24 +0300

    Add play button to stations + mini-stations

--- sprint4-frontend/src/cmps/StationPreview.jsx
+++ sprint4-frontend/src/cmps/StationPreview.jsx
@@ -1,11 +1,26 @@
 
 export function StationPreview({ station, isMini }) {
 
+    function handleClickPlay(ev) {
+        ev.stopPropagation()
+        console.log('Playing music!')
+    }
+
+    function getPlayBtn() {
+        return <button onClick={(ev) => { handleClickPlay(ev) }} className="preview-play-btn">{<svg width='24' height='24' viewBox="0 0 24 24">
+            <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"
+            >
+            </path>
+        </svg>} </button>
+    }
     return (
-        <article className="station-preview" >
+        <article className="station-preview">
+            {isMini && getPlayBtn()}
             <div className="img-container">
                 <img className="station-img" src={station.imgUrl} alt={`${station.name}`} />
+                {!isMini && getPlayBtn()}
             </div>
+
             <div className="station-content">
                 <h3 className="station-name">{station.name}</h3>
                 {!isMini && <h5 className="station-desc">{station.desc}</h5>}
